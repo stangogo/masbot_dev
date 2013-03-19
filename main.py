cdffc484c0f5ffb8edf71ea1742ed4892e38b3e6
stanliu <stanliu@localhost>
2013-03-19 16:26:55 +0800

    add a main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# -- coding: utf-8 --
+
+from masbot.config.global_settings import *
+from masbot.motion.adlink_fake import ADLinkMotion as Motion
+from masbot.actor.piston import Piston
+from masbot.actor.doubleaxis import DoubleAxis
+from masbot.actor.singleaxis import SingleAxis
+
+
+if __name__ == "__main__":
+    print('test')
